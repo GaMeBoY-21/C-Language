9a40c3d0cfd648f3c048fa97942c2253a5eb1a39
gameboy <kavalinikhilesh@gmail.com>
2025-05-19 22:59:05 +0530

    modified hello.c to print sum of first n natural numbers

--- practiceone/hello.c
+++ practiceone/hello.c
@@ -1,5 +1,15 @@
 #include<stdio.h>
+int sum(int n);
 int main(){
-    printf("hello");
+    int n = 10;
+    int r = sum(n);
+    printf("%d",r);
     return 0;
+}
+int sum(int n){
+    int sum = 0;
+    for(int i = 0;i<n;i++){
+        sum = sum + i;
+    }
+    return sum;
 }
